7200433157796d70b34354261f71ac903d724d9b
MAZG96 <48022915+MAZG96@users.noreply.github.com>
2019-05-29 16:15:04 +0200

    Add files via upload

--- alquicoche.sql
+++ alquicoche.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2019 a las 12:07:19
+-- Tiempo de generación: 29-05-2019 a las 16:13:04
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
@@ -55,21 +55,6 @@ CREATE TABLE `factura` (
   `tipo` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `factura`
---
-
-INSERT INTO `factura` (`id`, `cantidad`, `cuenta_destino`, `tarjeta_origen`, `reserva_id`, `tipo`) VALUES
-(108, 280, 'ES7620770024003102575766', '11111111111111111111111', 77, 'reserva'),
-(109, 224, '233443554656565767687878', 'ES7620770024003102575766', 77, 'cancelacion'),
-(110, 108, 'ES7620770024003102575766', '231542353454364235', 78, 'reserva'),
-(111, 86.4, '233443554656565767687878', 'ES7620770024003102575766', 78, 'cancelacion'),
-(112, 86.4, '233443554656565767687878', 'ES7620770024003102575766', 78, 'cancelacion'),
-(120, 30, 'ES7620770024003102575766', '231542353454364235', 86, 'reserva'),
-(121, 720, 'ES7620770024003102575766', '231542353454364235', 87, 'reserva'),
-(122, 24, '233443554656565767687878', 'ES7620770024003102575766', 86, 'cancelacion'),
-(123, 60, 'ES7620770024003102575766', '231542353454364235', 88, 'reserva');
-
 -- --------------------------------------------------------
 
 --
@@ -88,18 +73,6 @@ CREATE TABLE `reserva` (
   `problema` varchar(255) DEFAULT NULL,
   `seguro` bit(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `reserva`
---
-
-INSERT INTO `reserva` (`id`, `fechafin`, `fechaini`, `numero`, `precio`, `vehiculo_id`, `usuario_id`, `is_cancelada`, `problema`, `seguro`) VALUES
-(77, '2019-05-30', '2019-05-22', 1001, 280, 10, 6, b'1', NULL, b'0'),
-(78, '2019-05-25', '2019-05-22', 1014, 108, 11, 12, b'1', NULL, b'1'),
-(86, '2019-06-02', '2019-06-01', 1118, 30, 9, 12, b'1', NULL, b'0'),
-(85, '2019-05-22', '2019-05-15', 1105, 240, 9, 12, b'1', 'faro roto', b'0'),
-(87, '2019-05-31', '2019-05-23', 1131, 720, 12, 12, b'0', NULL, b'1'),
-(88, '2019-06-08', '2019-06-06', 1144, 60, 9, 12, b'0', NULL, b'0');
 
 -- --------------------------------------------------------
 
@@ -131,8 +104,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `addr`, `bio`, `birth`, `email`, `first_name`, `is_admin`, `last_name`, `password`, `profile_pic`, `tlf`, `username`, `cuenta_bancaria`, `tarjeta`, `is_Gerente`, `is_Gestor`) VALUES
-(6, 'Cadiz', 'admin', '1996-01-15', 'root@root.es', 'root', b'1', 'root', '$2a$11$2ykQ9b1mFNsXy0nflVPVDekN5kcXpauk1hhvFnsFUsNKtbcLouQzi', 'src\\webapp\\VAADIN\\img\\85-91.-Pau-Gasol-muestra-su-liderazgo-y-da-la-primera-victoria-a-los-Bulls.jpg', '633907611', 'root', '233443554656565767687878', '11111111111111111111111', 0, 1),
-(12, 'calle', 'gua', '2019-05-15', 'mail@mail.com', 'manolo', b'0', 'manolo', '$2a$11$MCnGJRmQxC/DAlSaaGLlVOr3UQqqNcnKNYQpM19ZjujuAuqwpL5bq', 'src\\webapp\\VAADIN\\img\\85-91.-Pau-Gasol-muestra-su-liderazgo-y-da-la-primera-victoria-a-los-Bulls.jpg', '214214214', 'man', '233443554656565767687878', '231542353454364235', 1, 0);
+(17, 'Chiclana', 'buen piloto', '1990-12-11', 'manutru@gmail.com', 'Manuel', b'0', 'Trujillo', '$2a$11$M1e2KsfNNG1vJxOG9lt1JeFAOEgYw3jC/BJfwDyuMX8C9sVdE1r6m', NULL, '623423542', 'manutru', NULL, '325235325H3H5423', 0, 0),
+(16, 'san fernando', 'conductor', '1965-05-16', 'juan@gmail.com', 'Juan', b'0', 'Sanchez', '$2a$11$HmPBH2wmvhaU2NP9bBjn7OktLcj6VpNbEuR5jDUxOITrUrYWwu6Ji', NULL, '352352356236', 'juan', NULL, '2342353253SADSAF', 0, 0),
+(14, 'cadiz', 'gestor', '1980-05-11', 'gestor@gmail.com', 'Gestor', b'0', 'Gestor', '$2a$11$3belA3WPwAn6U1IAUDzzZeLgl9vkSUylunSEMj4tfLowchv/sLlGW', NULL, '345678654', 'gestor', NULL, '35326457586867979789', 0, 1),
+(15, 'cadiz', 'gerente', '1977-05-30', 'gerente@gmail.com', 'gerente', b'0', 'gerente', '$2a$11$TRmUrqLjZ9J.pwTZZVoQP.kD0vumxPBhSg0eSRRRcI0xcCoYEwmoK', NULL, '325235235', 'gerente', NULL, '2354235356346436', 1, 0),
+(6, 'Cadiz', 'admin', '1996-01-15', 'root@root.es', 'root', b'1', 'root', '$2a$11$2ykQ9b1mFNsXy0nflVPVDekN5kcXpauk1hhvFnsFUsNKtbcLouQzi', 'src\\webapp\\VAADIN\\img\\85-91.-Pau-Gasol-muestra-su-liderazgo-y-da-la-primera-victoria-a-los-Bulls.jpg', '633907611', 'root', '233443554656565767687878', '11111111111111111111111', 0, 0),
+(18, 'Conil', 'Hola', '1996-09-12', 'anto@gmail.com', 'Antonio', b'0', 'Garcia', '$2a$11$3YlzbghXzY5wy.OQ3iR1aujuhWfqNpVcS.Q9jpAYfsA/FK3z4JQqa', NULL, '6432423423', 'anto', NULL, '4365436586543436436', 0, 0),
+(12, 'calle', 'gua', '2019-05-15', 'mail@mail.com', 'manolo', b'0', 'manolo', '$2a$11$MCnGJRmQxC/DAlSaaGLlVOr3UQqqNcnKNYQpM19ZjujuAuqwpL5bq', 'src\\webapp\\VAADIN\\img\\85-91.-Pau-Gasol-muestra-su-liderazgo-y-da-la-primera-victoria-a-los-Bulls.jpg', '214214214', 'man', '233443554656565767687878', '231542353454364235', 1, 0),
+(13, 'CALLE CADIZ', 'buen conductor', '1996-05-15', 'MAZG@GMAIL.COM', 'Miguel', b'0', 'Zara', '$2a$11$Ugt2pJ7NAyhzJLUFnHWaYutLMfN4PobuCh5SbzaI0aqBqropMySKK', 'src\\webapp\\VAADIN\\img\\a270d97b-b0a8-4cd3-b21a-0202655f2c0a.jpg', '654786543', 'MAZG', '233443554656565767687878', '31224235236325365', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -153,19 +132,22 @@ CREATE TABLE `vehiculo` (
   `precio_dia` int(11) NOT NULL,
   `oficina` varchar(255) NOT NULL,
   `galeria` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `usuario_id` bigint(20) DEFAULT NULL
+  `usuario_id` bigint(20) DEFAULT NULL,
+  `disponibilidad_ini` date DEFAULT NULL,
+  `disponibilidad_fin` date DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `vehiculo`
 --
 
-INSERT INTO `vehiculo` (`id`, `matricula`, `marca`, `estado`, `climatizador`, `gps`, `numero_de_plazas`, `tipo_transmision`, `carroceria`, `precio_dia`, `oficina`, `galeria`, `usuario_id`) VALUES
-(9, '1234', 'Opel', 'disponible', 1, 1, 5, 'automatico', 'okey', 30, 'cadiz', 'src\\webapp\\VAADIN\\img\\Opel-Corsa.jpg', 12),
-(10, '2881', 'Renault', 'disponible', 1, 1, 5, 'manual', 'tres volumenes', 35, 'Cádiz', 'src\\webapp\\VAADIN\\img\\auto-97-renault-kadjar-1-5-dci-zen.png', 12),
-(11, '3445', 'Seat', 'disponible', 1, 1, 7, 'Manual', 'monovolumen', 60, 'Chiclana', 'src\\webapp\\VAADIN\\img\\imageCrop1.resizeViewPort.crop.thumbnailAspectRatio.noScale.fileReference.jpg', 12),
-(12, '6887', 'Audi', 'disponible', 1, 0, 5, 'manual', 'turismo', 75, 'San Fernando', 'src\\webapp\\VAADIN\\img\\174732687_g.jpg', 12),
-(13, '4560', 'Volkswagen', 'averiado', 1, 1, 5, 'automatico', 'monovolumen', 45, 'Cádiz', 'src\\webapp\\VAADIN\\img\\412alOEi3NL._SR500,500_.jpg', 12);
+INSERT INTO `vehiculo` (`id`, `matricula`, `marca`, `estado`, `climatizador`, `gps`, `numero_de_plazas`, `tipo_transmision`, `carroceria`, `precio_dia`, `oficina`, `galeria`, `usuario_id`, `disponibilidad_ini`, `disponibilidad_fin`) VALUES
+(9, '1234', 'Opel', 'disponible', 1, 1, 5, 'automatico', 'okey', 30, 'cadiz', 'src\\webapp\\VAADIN\\img\\Opel-Corsa.jpg', 14, '2019-05-19', '2019-05-31'),
+(10, '2881', 'Renault', 'disponible', 1, 1, 5, 'manual', 'tres volumenes', 35, 'Cádiz', 'src\\webapp\\VAADIN\\img\\auto-97-renault-kadjar-1-5-dci-zen.png', 12, NULL, NULL),
+(11, '3445', 'Seat', 'disponible', 1, 1, 7, 'Manual', 'monovolumen', 60, 'Chiclana', 'src\\webapp\\VAADIN\\img\\imageCrop1.resizeViewPort.crop.thumbnailAspectRatio.noScale.fileReference.jpg', 12, NULL, NULL),
+(12, '6887', 'Audi', 'disponible', 1, 0, 5, 'manual', 'turismo', 75, 'San Fernando', 'src\\webapp\\VAADIN\\img\\174732687_g.jpg', 12, NULL, NULL),
+(13, '4560', 'Volkswagen', 'averiado', 1, 1, 5, 'automatico', 'monovolumen', 45, 'Cádiz', 'src\\webapp\\VAADIN\\img\\412alOEi3NL._SR500,500_.jpg', 12, NULL, NULL),
+(15, '4654', 'Opel', 'disponible', 1, 1, 6, 'automatico', 'monovolumen', 67, 'cadiz', 'src\\webapp\\VAADIN\\img\\auto-97-renault-kadjar-1-5-dci-zen.png', 14, '2019-05-29', '2019-05-27');
 
 --
 -- Índices para tablas volcadas
@@ -220,25 +202,25 @@ ALTER TABLE `cuentageneral`
 -- AUTO_INCREMENT de la tabla `factura`
 --
 ALTER TABLE `factura`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
 
 --
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculo`
 --
 ALTER TABLE `vehiculo`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
